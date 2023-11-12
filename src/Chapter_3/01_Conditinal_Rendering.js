32b64eacf2cd6a08fd1f003aabaa8fa6e92a5b01
Sumit Tupe <sumittupe9623@gmail.com>
2023-11-12 18:06:34 +0530

    Chapter 4 Added

--- src/Chapter_3/01_Conditinal_Rendering.js
+++ src/Chapter_3/01_Conditinal_Rendering.js
@@ -3,6 +3,7 @@ import "./01_Conditional_Rendering.css";
 // In this Condition is applied in APP.js for verified channel or not
 // For list there is one array of videos in APP.js it will iterate and will form output for multiple videos
 function Video({
+  id,
   title,
   children,
   channel = "Sumit Tupe",
@@ -10,7 +11,7 @@ function Video({
   time,
   verified = false,
 }) {
-  let img_url = "https://picsum.photos/id/1/160/90";
+  let img_url = `https://picsum.photos/id/${id}/160/90`;
   return (
     <div className="Thumbnail">
       <img src={img_url} alt="Katherine_Johnson" />
