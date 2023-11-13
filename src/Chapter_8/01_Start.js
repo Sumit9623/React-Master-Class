2e150f0277f6dcfd38a92389b413fae30b9eff1c
Sumit Tupe <sumittupe9623@gmail.com>
2023-11-13 17:58:41 +0530

    Chapter 8 Added

--- src/Chapter_8/01_Start.js
+++ src/Chapter_8/01_Start.js
@@ -1,3 +1,4 @@
 
 
-//instead of passing function to upadate state through Props to childern we can pass single dispatch which can handle multiple calls
+// instead of passing function to upadate state through Props to childern we can pass single dispatch which can handle multiple calls
+// see the changes in App.js where we have used useReduucers instead of useState
